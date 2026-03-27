b7299abe0df432ca9da1a6bf99ff8a38d9551257
Chippo156 <vovannghiahiep150623@gmail.com>
2026-03-27 16:12:28 +0700

    [chippo] [root] [workspace] setup nx workspace actions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    './apps/**/*.{html,ts}',
+    './libs/**/*.{html,ts}', // 🔥 QUAN TRỌNG
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [require('@tailwindcss/line-clamp')],
 };
